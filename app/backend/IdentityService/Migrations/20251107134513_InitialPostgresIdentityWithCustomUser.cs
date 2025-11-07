153a5a07486f06ab68124aa1b54201b4f9f7f13d
hype258 <141255923+hype258@users.noreply.github.com>
2025-11-07 14:46:11 +0100

    dodanie  danych uzytkownika w formularzu

--- app/backend/IdentityService/Migrations/20251107134513_InitialPostgresIdentityWithCustomUser.cs
+++ app/backend/IdentityService/Migrations/20251107134513_InitialPostgresIdentityWithCustomUser.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IdentityService.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialPostgresIdentity : Migration
+    public partial class InitialPostgresIdentityWithCustomUser : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -31,6 +31,10 @@ namespace IdentityService.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    CompanyId = table.Column<int>(type: "integer", nullable: false),
                     UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
